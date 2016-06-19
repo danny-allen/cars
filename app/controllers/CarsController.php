76d442056a47947ce35a3000f78597dff78eec9d
dannyallen16 <danny@noise-maker.co.uk>
2016-06-19 17:41:48 +0100

    Index method now returning cars in the cars controller.

--- app/controllers/CarsController.php
+++ app/controllers/CarsController.php
@@ -1,10 +1,39 @@
 <?php
+ 
+use Phalcon\Mvc\Model\Criteria;
+use Phalcon\Paginator\Adapter\Model as Paginator;
 
 class CarsController extends AbstractController
 {
 
     public function indexAction()
     {
+        $this->persistent->parameters = null;
+        
+        $numberPage = 1;
+        $parameters["order"] = "id";
+
+
+        $posts = Cars::find($parameters);
+        
+        if (count($posts) == 0) {
+            $this->flash->notice("The search did not find any cars.");
+
+            $this->dispatcher->forward(array(
+                "controller" => "cars",
+                "action" => "index"
+            ));
+
+            return;
+        }
+
+        $paginator = new Paginator(array(
+            'data' => $posts,
+            'limit'=> 10,
+            'page' => $numberPage
+        ));
+
+        $this->view->page = $paginator->getPaginate();
 
     }
 }
