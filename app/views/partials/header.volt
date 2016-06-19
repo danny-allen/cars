a74c11442ca8be4535227c57a4c78db6399ec32e
dannyallen16 <danny@noise-maker.co.uk>
2016-06-19 02:19:19 +0100

    Added mobile nav icon and js animation, which should really be done with css.

--- app/views/partials/header.volt
+++ app/views/partials/header.volt
@@ -1,3 +1,6 @@
 <header class="header">
+	<div class="header__mobile-icon" ui-mobile-icon>
+		<i class="fa fa-bars" aria-hidden="true"></i>
+	</div>
 	{{ image("/images/logo.png", "alt": "Logo", "class": "header__logo") }}
 </header>
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,4 +10,11 @@ var Cars = Cars || {};
     //initialise foundation
 	$(document).foundation();
 
+	//nav mobile
+	$('[ui-mobile-icon]').on('click', function() {
+
+		//bit lazy - should really do some css animation here
+		$('.side-menu').slideToggle();
+	});
+
 })(jQuery, window, document);
