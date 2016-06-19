d2a95b4351cf0ac0d454ce083c7d33bbea2eed5f
dannyallen16 <danny@noise-maker.co.uk>
2016-06-19 11:56:06 +0100

    Returning posts within the post controller index method.

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -12,6 +12,30 @@ class PostsController extends ControllerBase
     public function indexAction()
     {
         $this->persistent->parameters = null;
+        
+        $numberPage = 1;
+        $parameters["order"] = "id";
+
+
+        $posts = Posts::find($parameters);
+        if (count($posts) == 0) {
+            $this->flash->notice("The search did not find any posts");
+
+            $this->dispatcher->forward(array(
+                "controller" => "posts",
+                "action" => "index"
+            ));
+
+            return;
+        }
+
+        $paginator = new Paginator(array(
+            'data' => $posts,
+            'limit'=> 10,
+            'page' => $numberPage
+        ));
+
+        $this->view->page = $paginator->getPaginate();
     }
 
 
